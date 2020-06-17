75f66926d013b31e84e59b42cf0a0d7a1da781b1
rinaldycodes <rinaldy.ramadhan999@gmail.com>
2020-06-17 12:33:23 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    let $btns = $('.project-area .button-group button');
+
+    $btns.click(function(e){
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter:selector
+        });
+        return false;
+    });
+
+
+    // Owl Carousel
+    $('.site-main .about-me-area .owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            544:{
+                items:2
+            }
+        }
+    })
+
+    // STICKY NAVIGATION MENU
+
+    let nav_offset_top = $('.header_area').height()+50;
+
+    function navbarFixed(){
+        if ($('.header_area').length) {
+            $(window).scroll(function(){
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                }else{
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
+    
+});
